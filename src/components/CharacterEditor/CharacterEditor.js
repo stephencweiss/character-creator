e2b55239fc9274c6fb461a96ad67214b87ebdbe7
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-09-17 10:26:18 -0700

    Absolutley position character
    exercise 2 solution - absolutely position the character to the right and
    curb the width of the description and controls

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -38,6 +38,7 @@ function App() {
             controls below. What sort of adventure will you embark on?{' '}
           </p>
         </header>
+
         <div className={styles.controlColumn}>
           <ControlPane
             title="Bodies"
