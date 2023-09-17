36ca64c9f75fc98747248093b082f5d471fff6fe
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-09-17 11:19:27 -0700

    Add a perspective effect with a grey background

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspective}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -89,6 +90,7 @@ function App() {
           clothesColor={clothesColor}
         />
       </div>
+
     </main>
   );
 }
